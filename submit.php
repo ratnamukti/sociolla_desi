1c4e457e06789a1c3c4cd586d612fcfd25e50a9e
ratnamukti <desi.rmu@gmail.com>
2016-04-20 01:02:31 +0700

    initial commit with all code pushed

---
+++ submit.php
@@ -0,0 +1,12 @@
+<?php 
+
+if(isset($_POST["name"]) || isset($_POST["description"])) {
+
+	http_response_code(200);
+    echo "name: ". $_POST['name']. "<br />";
+    echo "description: ". $_POST['description'];
+      exit();
+   }
+
+
+?>
